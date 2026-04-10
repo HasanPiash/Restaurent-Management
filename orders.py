145f70408d08d5963c107813fb012e06f0ae2991
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-10 19:12:06 +0600

    Update orders.py

--- orders.py
+++ orders.py
@@ -2,6 +2,7 @@ class Order:
     def __init__(self) ->None:
         self.items={}
 
+    
     def add_item(self, item):
         if item in self.items:
             # If The item is already in cart
@@ -9,13 +10,16 @@ class Order:
         else:
             self.items[item]=item.quantity  #If item is not in cart
 
+    
     def remove(self, item):
         if item in self.items:
             del self.items[item]
 
+    
     @property
     def total_price(self):
         return sum(item.price * quantity for item, quantity in self.items.items())
 
+    
     def clear(self):
         self.items={}
