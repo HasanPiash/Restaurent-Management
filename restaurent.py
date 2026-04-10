d27ed3b820a9fee1373474101bd85d12afaa07fc
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-10 19:13:57 +0600

    Update restaurent.py

--- restaurent.py
+++ restaurent.py
@@ -6,10 +6,13 @@ class Restaurent:
         self.employees=[]
         self.menu=Menu()
 
+        
     def add_employee(self,employee):
         self.employees.append(employee)
 
+         
     def view_employee(self):
         print("Employee List!!")
+        
         for emp in self.employees:
             print(emp.name, emp.email, emp.phone, emp.address)
