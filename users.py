dd9a00a50a271010725df3f7fcc8846868e46308
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-04-10 19:49:06 +0600

    Update users.py

--- users.py
+++ users.py
@@ -1,6 +1,7 @@
 # Customer
 # Employee
 # Admin
+       
 from abc import ABC
 from orders import Order
 class User(ABC):
@@ -16,11 +17,14 @@ class Customer(User):
         super().__init__(name, phone, email, address)
         self.cart = Order()
 
+    
     def view_menu(self, restaurent):
         restaurent.menu.show_menu()
 
+    
     def add_to_cart(self, restaurent, item_name, quantity):
         item = restaurent.menu.find_item(item_name)
+        
         if item:
             if quantity > item.quantity:
                 print("Item quantity exceeded!!")
@@ -31,6 +35,7 @@ class Customer(User):
         else:
             print("Item not found")
 
+    
     def view_cart(self):
         print("**View Cart**")
         print("Name\tPrice\tQuantity")
@@ -38,10 +43,12 @@ class Customer(User):
             print(f"{item.name}\t{item.price}\t{quantity}")
         print(f"Total Price : {self.cart.total_price}")
 
+    
     def pay_bill(self):
         print(f"Total {self.cart.total_price} paid successfully")
         self.cart.clear()
-        
+
+
 class Employee(User):
     def __init__(self, name, email, phone, address, age, designation, salary):
         super().__init__(name, phone, email, address)
@@ -49,24 +56,31 @@ class Employee(User):
         self.designation = designation
         self.salary = salary
 
+
 # emp = Employee("rahim", "rahim@gmail.com", 1279787, "Dhaka", 23, "Chef", 12000)
 # print(emp.name)
+
 
 class Admin(User):
     def __init__(self, name, email, phone, address):
         super().__init__(name, phone, email, address)
 
+    
     def add_employee(self, restaurent, employee):
         restaurent.add_employee(employee)
 
+        
     def view_employee(self, restaurent):
         restaurent.view_employee()
 
+      
     def add_new_item(self, restaurent, item):
         restaurent.menu.add_menu_item(item)
 
+    
     def remove_item(self, restaurent, item):
         restaurent.menu.remove_item(item)
+
     
     def view_menu(self, retaurent):
         retaurent.menu.show_menu()
